5d1b3372a73af01449226a19fb840c97fc098deb
skipglide <gravesdavid133@gmail.com>
2024-03-28 16:49:50 -0500

    Added home view

--- dtest/urls.py
+++ dtest/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", views.home, name='home'),
+]   + staticfiles_urlpatterns()\
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

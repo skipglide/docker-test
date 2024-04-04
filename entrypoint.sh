29e935b348359eea58bf839be62521b73bccd3fb
skipglide <gravesdavid133@gmail.com>
2024-04-04 06:41:04 -0500

    fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Apply database migrations"
 python manage.py migrate
 echo "Start gunicorn server"
-gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
+gunicorn mydtest.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"

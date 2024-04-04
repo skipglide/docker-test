e1fbcc6ad7b7a7d899141a51a0be26c55141b86f
skipglide <gravesdavid133@gmail.com>
2024-04-04 07:06:44 -0500

    AAHAHHAH

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Apply database migrations"
 python manage.py migrate
 echo "Start gunicorn server"
-gunicorn mydtest.wsgi:application --bind 0.0.0.0:8000
+gunicorn dtest.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"

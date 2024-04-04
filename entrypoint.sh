883c4b1870aeda9227854c26979d48c2c10892c9
skipglide <gravesdavid133@gmail.com>
2024-04-04 10:08:09 -0500

    run without gunicorn to see

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Apply database migrations"
 python manage.py migrate
 echo "Start gunicorn server"
-gunicorn dtest.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
